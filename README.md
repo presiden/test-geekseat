e66c8fb724a80642fb6853c720f94e41696c229b
prasidyo <prasidyo@gmail.com>
2021-05-21 10:21:33 +0700

    change to web application

--- README.md
+++ README.md
@@ -1,4 +1,25 @@
-####### How to run java application (Windows)
+##### How to run java application without build jar (Windows)
+###### Without build jar
 1. Open command prompt in project folder 
-2. Compile java source code, type: mvnw compile package
-3. Run application: java -jar target/test-geekseat-0.0.1-SNAPSHOT.jar
+2. Run application, type: mvnw spring-boot:run
+
+###### With build jar
+1. Open command prompt in project folder 
+2. Compile java source code, type: mvnw clean package
+3. Run application: java -jar target/test-geekseat.jar
+
+Sample: 
+- url: localhost:8080/calculate
+- method: POST
+
+- body request : 
+{
+    "A": {
+        "ageOfDeath": 10,
+        "yearOfDeath": 11
+    },
+    "B": {
+        "ageOfDeath": 10,
+        "yearOfDeath": 54
+    }
+}
--- src/main/java/com/example/demo/DemoApplication.java
+++ src/main/java/com/example/demo/DemoApplication.java
@@ -1,27 +1,13 @@
 package com.example.demo;
 
-import java.util.HashMap;
-import java.util.Map;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+@SpringBootApplication
 public class DemoApplication {
 
 	public static void main(String[] args) {
-		Map<String, Map<String, Integer>> villagers = new HashMap<>();
-		Map<String, Integer> villager1 = new HashMap<>();
-		Map<String, Integer> villager2 = new HashMap<>();
-
-		villager1.put("ageOfDeath", 10);
-		villager1.put("yearOfDeath", 10);
-		villagers.put("A", villager1);
-
-		villager2.put("ageOfDeath", 10);
-		villager2.put("yearOfDeath", 17);
-		villagers.put("B", villager2);
-
-		ServiceApplicationImpl serviceApplication = new ServiceApplicationImpl();
-		Map<String, Object> result = serviceApplication.calculate(villagers);
-
-		System.out.println("Average people killed: " + result.get("avgPeopleKilled"));
+		SpringApplication.run(DemoApplication.class, args);
 	}
 
 }
